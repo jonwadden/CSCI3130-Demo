379e6168fe3c176649b4f7b0f28bfbd87d7eb2df
Jon Wadden <jon.wadden@dal.ca>
2018-05-15 11:35:28 -0300

    Added Text Field, Button Added UI element constraints Added functionality: - When button is pressed, the text entered in the text field replaces the label text at the centre of the screen

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Respond to button press
+    public void buttonPress(View view){
+      EditText textField = findViewById(R.id.editText);
+      String text = textField.getText().toString();
+      TextView label = findViewById(R.id.label);
+      label.setText(text);
+    }
+
 }
